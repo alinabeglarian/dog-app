1789a6eb63df2dc43bc8e9d0d6cb3daef02f1e50
alinabeglarian <alinabeglarian@gmail.com>
2019-06-19 17:40:12 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import store from './store';
 import { Route } from 'react-router-dom'
 import DogsListContainer from './components/DogsListContainer';
 import GameOneContainer from './components/GameOneContainer'
-
+import { Link } from 'react-router-dom'
+import DogImagesContainer from './components/DogImagesContainer'
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
         <div className='App'>
-          <Route exact path='/' component={DogsListContainer} />
+          <main>
+          <Link to="/list">Dog List</Link>
+          <Route exact path="/list" component={DogsListContainer} />
+          <Route exact path="/breed/:dogBreed" component={DogImagesContainer} />  
           <Route exact path='/gameone/' component={GameOneContainer} />
+          </main>
         </div>
       </Provider>
     );
   }
 }
+
 export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import dogBreed from './dogBreed'
+import dogImages from './dogImages'
 import dogGameOne from './dogGameOne'
+
 
 export default combineReducers({
   dogBreed,
+  dogImages,
   dogGameOne
 })
