8e14514d50ca2f4cce5de4c5b108f61933d1ce5c
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-21 09:47:03 +0200

    Adding CSS to the images

--- src/components/DogImages.js
+++ src/components/DogImages.js
@@ -7,8 +7,10 @@ export default function DogImages (props) {
     return (
       <div className="dog-breed-images">
         <h1>Dogs Breed Images</h1>
-        <p>{ props.images && props.images.map(url => <img src={ url } key={url} alt="Dog" />) }
-      { !props.images && 'Loading...' }</p> 
+
+        <div className='ImgRandDog ImgRandDog-cover'>{ props.images && props.images.map(url => <img src={ url } key={url} alt="Dog" />) }
+      { !props.images && 'Loading...' }
+      </div> 
         <p><Link to="/list">Go back to the index</Link></p>
         <div>
       
