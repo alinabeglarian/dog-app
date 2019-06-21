a47141543dbeeb466c5f6b4cedc4fd3f1ce30a30
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-21 10:53:50 +0200

    Adding the Links to the games on App.js file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import GameOneContainer from './components/GameOneContainer';
 import { Link } from 'react-router-dom';
 import DogImagesContainer from './components/DogImagesContainer';
 import GameTwoContainer from './components/GameTwoContainer';
+import GameThreeContainer from './components/GameThreeContainer';
 
 class App extends React.Component {
   render() {
@@ -17,17 +18,17 @@ class App extends React.Component {
         <div className='App'>
           <main>
             <Link to='/list'>Dog List</Link>
+            
             <br />
+            <Link to='/gameone'>Game One</Link>
             <Link to='/gametwo'>Game Two</Link>
+            <Link to='/gameThree'>Game Three</Link>
 
             <Route exact path='/list' component={DogsListContainer} />
-            <Route
-              exact
-              path='/breed/:dogBreed'
-              component={DogImagesContainer}
-            />
+            <Route exact path='/breed/:dogBreed' component={DogImagesContainer} />
             <Route exact path='/gameone/' component={GameOneContainer} />
             <Route exact path='/gametwo/' component={GameTwoContainer} />
+            <Route exact path='/gamethree/' component={GameThreeContainer} />
           </main>
         </div>
       </Provider>
